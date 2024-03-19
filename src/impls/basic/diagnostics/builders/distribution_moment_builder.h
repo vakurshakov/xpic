33eda4d8c6746f74e07647f3fb2e6a34eb6a995a
vladislav <crimsonschist@gmail.com>
2024-03-19 19:18:58 +0700

    velocity distribution added; implementing usage_message()

--- src/impls/basic/diagnostics/builders/distribution_moment_builder.h
+++ src/impls/basic/diagnostics/builders/distribution_moment_builder.h
@@ -16,7 +16,25 @@ public:
 
 private:
   constexpr std::string usage_message() const override {
-    return "\n" "Usage is unspecified yet.";
+    return "\n"
+      "Usage: The structure of the distribution_moment diagnostics description\n"
+      "\"moment_name\": -- Name of the collected distribution moment, listed after description.\n"
+      "{\n"
+      "  \"sort\": \"electrons\", -- Particles sort name, must match names from particles section.\n"
+      "  \"start\": [ox, oy, oz], -- Starting point of a diagnostic in _global_ coordinates.\n"
+      "  \"size\":  [sx, sy, sz], -- Sizes of a diagnosed region along each coordinate in _global_ coordinates.\n"
+      "  \"dp\":    [dx, dy, dz]  -- Grid steps of a diagnosed region along each coordinate in _global_ coordinates.\n"
+      "}\n"
+      "\n"
+      "Available distribution moment names are:\n"
+      "  - density (zeroth_moment for x, y, z);\n"
+      "  - velocity (zeroth_moment for Vx, Vy, Vz);\n"
+      "  - Vx_moment and moments of Vy, Vz, Vr, Vphi;\n"
+      "  - mVxVx_moment and right-first combinations with Vy, Vz, Vr, Vphi, i.e. mVxVy, mVxVz, ...\n"
+      "\n"
+      "The region, described by \"start\", \"size\" and \"dp\", should use units coherent with distribution moment:\n"
+      "  - density, Vx_moment, mVxVy_moment, etc. collect moments onto (x, y, z) coordinates in units of c/w_pe.\n"
+      "  - velocity collects zeroth moment onto (Vx, Vy, Vz) coordinates in units of c.";
   }
 
   std::string moment_name;
