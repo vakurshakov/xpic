5d7126bd09760cc6450efd02287580cb77dc66aa
vladislav <crimsonschist@gmail.com>
2024-06-16 01:07:19 +0700

    some default configurations

--- doc/conf.py
+++ doc/conf.py
@@ -4,17 +4,29 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'xpic'
 copyright = '2024, Vladislav Kurshakov'
 author = 'Vladislav Kurshakov'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = []
 
-templates_path = ['_templates']
 exclude_patterns = []
+
+# Sphinx will warn about all references where the target cannot be found.
+nitpicky = True
+
+highlight_language = 'cpp'
+primary_domain = 'cpp'
+
+latex_elements = {
+  # Font size in points, 12pt is the maximum available
+  'pointsize': '12pt',
+
+  # Removes empty pages; no distinction on odd-even pages
+  'extraclassoptions': 'openany,oneside',
+
+  # Uncomment to enable Cyrillic fonts support
+  # 'fontenc': '\\usepackage[T2A,T1]{fontenc}',
+}
