c0788cb634bef951967739eab9e6e0e5fd29b8fb
vladislav <crimsonschist@gmail.com>
2024-06-26 15:41:09 +0700

    log.tpp is removed

--- src/utils/log.h
+++ src/utils/log.h
@@ -8,31 +8,47 @@
 #include <spdlog/spdlog.h>
 
 class Log {
+private:
+  template<typename... Args>
+  using format_t = spdlog::format_string_t<Args...>;
+
 public:
   static void init(const std::string& filename);
 
-  template <typename... Args>
-  static void trace(MPI_Comm comm, spdlog::format_string_t<Args...> fmt, Args&&... args);
+  template<typename... Args>
+  static void trace(MPI_Comm comm, format_t<Args...> fmt, Args&&... args) {
+    log(comm, spdlog::level::trace, fmt, std::forward<Args>(args)...);
+  }
 
-  template <typename... Args>
-  static void info(MPI_Comm comm, spdlog::format_string_t<Args...> fmt, Args&&... args);
+  template<typename... Args>
+  static void info(MPI_Comm comm, format_t<Args...> fmt, Args&&... args) {
+    log(comm, spdlog::level::info, fmt, std::forward<Args>(args)...);
+  }
 
-  template <typename... Args>
-  static void warn(MPI_Comm comm, spdlog::format_string_t<Args...> fmt, Args&&... args);
+  template<typename... Args>
+  static void warn(MPI_Comm comm, format_t<Args...> fmt, Args&&... args) {
+    log(comm, spdlog::level::warn, fmt, std::forward<Args>(args)...);
+  }
 
-  template <typename... Args>
-  static void error(MPI_Comm comm, spdlog::format_string_t<Args...> fmt, Args&&... args);
+  template<typename... Args>
+  static void error(MPI_Comm comm, format_t<Args...> fmt, Args&&... args) {
+    log(comm, spdlog::level::err, fmt, std::forward<Args>(args)...);
+  }
 
   static void flush(MPI_Comm comm);
 
 private:
-  template <typename... Args>
-  static void log(MPI_Comm comm, spdlog::level::level_enum lvl, spdlog::format_string_t<Args...> fmt, Args&&... args);
+  template<typename... Args>
+  static void log(MPI_Comm comm, spdlog::level::level_enum lvl, format_t<Args...> fmt, Args &&...args) {
+    assert(comm != MPI_COMM_NULL && "Logger::log() called with MPI_COMM_NULL");
+
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+    if (rank == 0) { logger_->log(lvl, fmt, std::forward<Args>(args)...); }
+  }
 
   static std::shared_ptr<spdlog::logger> logger_;
 };
-
-#include "log.tpp"
 
 #if LOGGING
 #define LOG_INIT(filename) ::Log::init(filename)
