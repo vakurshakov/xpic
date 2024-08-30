18a23bc0f03653ca988148c9a08e111d7a76972a
vladislav <crimsonschist@gmail.com>
2024-08-30 18:06:04 +0700

    cmake support!

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 usage() { echo "Usage: $0 <config.json>" 1>&2; exit 1; }
 
+export MPI_NUM_PROC=1
+
 export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 export OMP_NUM_THREAD=1
@@ -14,4 +16,4 @@ if [[ $# == 0 ]]; then
   usage
 fi
 
-mpiexec ./bin/simulation.out $1
+mpiexec -np $MPI_NUM_PROC ./build/xpic $1
