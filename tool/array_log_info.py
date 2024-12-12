b6c7c3b7b2082996ffb6e4c02139533bab684817
vakurshakov <crimsonschist@gmail.com>
2024-12-12 17:33:09 +0700

    Figure 12a, 12b. Some problem (b) figure: not exactly z'(t) = z(t) - 0.01 * t^2

--- tool/array_log_info.py
+++ tool/array_log_info.py
@@ -47,39 +47,53 @@ def read_text(filename):
 
 
 def get_args(c, m):
-    return {"c": c, "lw": 1, "emphasis": {"c": c, "marker": m, "s": 1, "alpha": 0.8}}
+    return {"c": c, "lw": 1, "emphasis": {"c": c, "marker": m, "s": 10, "alpha": 0.8}}
 
-r_args = get_args("red", "o")
+r_args = get_args("red", "x")
 b_args = get_args("blue", "s")
-g_args = get_args("green", "d")
-p_args = get_args("purple", "<")
+g_args = get_args("green", "+")
+p_args = get_args("purple", "$O$")
 
-tmax = 500
+tmax = 150
 
-dt = 0.1975
+dt = 0.5
 ts = np.arange(0, tmax) * dt
-v0x = 0.1
-vd = 0.004
-# zd = ts * vd
 
-prefix = "./tests/chin_output/chin_crossed_fields"
+prefix = "./tests/chin_output/chin_polarization"
 
-data = read_text(f"{prefix}_EB1A.txt")
-plt.scatter(data["y"][:tmax] - (vd * ts), data["z"][:tmax], **g_args["emphasis"])
-data = read_text(f"{prefix}_EB1B.txt")
-plt.scatter(data["y"][:tmax] - (vd * ts), data["z"][:tmax], **r_args["emphasis"])
-data = read_text(f"{prefix}_EBLF.txt")
-plt.scatter(data["y"][:tmax] - (vd * ts), data["z"][:tmax], **p_args["emphasis"])
-data = read_text(f"{prefix}_EB2B.txt")
-plt.scatter(data["y"][:tmax] - (vd * ts), data["z"][:tmax], **b_args["emphasis"])
+l = "a"
+if l == "a":
+    data = read_text(f"{prefix}_EB1A.txt")
+    plt.scatter(data["y"][:tmax], data["z"][:tmax], **g_args["emphasis"])
+    data = read_text(f"{prefix}_EB1B.txt")
+    plt.scatter(data["y"][:tmax], data["z"][:tmax], **r_args["emphasis"])
+    data = read_text(f"{prefix}_EBLF.txt")
+    plt.scatter(data["y"][:tmax], data["z"][:tmax], **p_args["emphasis"])
+    data = read_text(f"{prefix}_EB2B.txt")
 
-# plot_parametric(v0x * ts, ts * vd, c="black", lw=1, alpha=0.8)
+    b_args.pop("emphasis")
+    plot_parametric(data["y"][:tmax], data["z"][:tmax], **b_args)
+    plt.xlim((-0.1, 0.1))
+    plt.ylim((-5, 60))
 
+elif l == "b":
+    data = read_text(f"{prefix}_EB1A.txt")
+    plt.plot(ts, data["z"][:tmax] - (0.01 * ts * ts), lw=1, c="g") #, **g_args["emphasis"])
+    data = read_text(f"{prefix}_EB1B.txt")
+    plt.plot(ts, data["z"][:tmax] - (-0.01 * ts + 0.01 * ts * ts), lw=1, c="r") #, **r_args["emphasis"])
+    data = read_text(f"{prefix}_EBLF.txt")
+    plt.plot(ts, data["z"][:tmax] - (-0.01 * ts + 0.01 * ts * ts), lw=1, c="purple") #, **p_args["emphasis"])
+    data = read_text(f"{prefix}_EB2B.txt")
+    plt.plot(ts, data["z"][:tmax] - (-0.005 * ts + 0.01 * ts * ts), lw=1, c="b") #, **b_args)
+
+    rg = 0.001
+    plt.plot([ts[0], ts[-1]], [-rg, -rg], lw=1, c="black", alpha=0.8)
+    plt.plot([ts[0], ts[-1]], [+rg, +rg], lw=1, c="black", alpha=0.8)
+    Rg = 0.052467
+    plt.plot([ts[0], ts[-1]], [-Rg, -Rg], lw=1, c="black", alpha=0.8)
+    plt.plot([ts[0], ts[-1]], [+Rg, +Rg], lw=1, c="black", alpha=0.8)
+    plt.xlim((0, 50))
 
 plt.title("Gyro orbits")
 plt.grid(c="grey", alpha=0.6)
-# plt.xlim((-1, 11))
-# plt.ylim((-0.1, 0.5))
-plt.xlim((-0.1, 0.1))
-plt.ylim((-0.1, 0.1))
 plt.show()
