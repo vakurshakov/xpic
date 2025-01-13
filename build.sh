5765694b23dc29f315638e045279d8c640a71fa6
vakurshakov <crimsonschist@gmail.com>
2025-01-13 13:23:40 +0700

    using environmental variables for directories of mpi, json, petsc

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export MPI_DIR="/opt/mpich/"
+export JSON_DIR="external/json"
+export PETSC_DIR="external/petsc"
+
 cmake -S . -B build/ -D CMAKE_BUILD_TYPE=DEBUG
 
 cmake --build build/ --parallel 8
