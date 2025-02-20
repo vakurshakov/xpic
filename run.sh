e72ca3edd387ff0b72e688e9aa9b61196c367b83
vakurshakov <crimsonschist@gmail.com>
2025-02-20 18:50:29 +0700

    finalizing omp and mpi configuration with config.json

--- run.sh
+++ run.sh
@@ -2,47 +2,64 @@
 
 source ./header.sh
 
-usage() { echo "Usage: $0 <config.json>" 1>&2; exit 1; }
+if [[ $# == 0 ]]; then
+  echo "Empty argument list, at least configuration file is required"
+  echo "Usage: $0 <config.json>" 1>&2;
+  exit 1;
+fi
 
 MPI_NUM_PROC=1
+
+# see https://github.com/pmodels/mpich/issues/4616
+# see https://github.com/pmodels/mpich/blob/main/doc/wiki/how_to/Using_the_Hydra_Process_Manager.md#process-core-binding
 HYDRA_BINDING=none
 HYDRA_MAPPING=none
 
 OMP_NUM_THREADS=1
-OMP_PLACES=cores
-OMP_PROC_BIND=spread
+OMP_PROC_BIND=true # see https://www.openmp.org/spec-html/5.0/openmpse52.html
+OMP_PLACES=cores   # see https://www.openmp.org/spec-html/5.0/openmpse53.html
 
-while read -r line; do
+#                           # Can help to inspect thread migration
+OMP_DISPLAY_ENV=false       # [true | false | verbose], see https://www.openmp.org/spec-html/5.0/openmpse60.html
+OMP_DISPLAY_AFFINITY=false  # [true | false],           see https://www.openmp.org/spec-html/5.0/openmpse61.html
+
+parse() {
+  line=$(echo $1 | sed s/\"//g)
+  line=${line#*: }
+  line=${line%,}
+  echo $line
+}
+
+# Reading mpi and omp configuration from config.json
+while read line; do
   case $line in
-    *num_threads*)
-      line=${line#*: }
-      line=${line%,}
-      OMP_NUM_THREADS=$line
-      ;;
     *da_processors*)
-      line=${line#*: }
-      line=${line%,}
-      MPI_NUM_PROC=$(( $MPI_NUM_PROC * $line ))
-      ;;
+      MPI_NUM_PROC=$(( $MPI_NUM_PROC * $(parse "$line") )) ;;
+    *binding*)
+      HYDRA_BINDING=$(parse "$line") ;;
+    *mapping*)
+      HYDRA_MAPPING=$(parse "$line") ;;
+    *num_threads*)
+      OMP_NUM_THREADS=$(parse "$line") ;;
+    *places*)
+      OMP_PLACES=$(parse "$line") ;;
+    *proc_bind*)
+      OMP_PROC_BIND=$(parse "$line") ;;
+    *display_env*)
+      OMP_DISPLAY_ENV=$(parse "$line") ;;
+    *display_affinity*)
+      OMP_DISPLAY_AFFINITY=$(parse "$line") ;;
   esac
 done < config.json
 
-export MPI_NUM_PROC
-export HYDRA_BINDING
-export HYDRA_MAPPING
+export HYDRA_BINDING=$HYDRA_BINDING
+export HYDRA_MAPPING=$HYDRA_MAPPING
 
-export OMP_NUM_THREADS
-export OMP_PLACES
-export OMP_PROC_BIND
-
-# To inspect thread migration
-# export OMP_DISPLAY_ENV=verbose
-# export OMP_DISPLAY_AFFINITY=true
-
-if [[ $# == 0 ]]; then
-  echo "Empty argument list, at least configuration file is required"
-  usage
-fi
+export OMP_NUM_THREADS=$OMP_NUM_THREADS
+export OMP_PLACES=$OMP_PLACES
+export OMP_PROC_BIND=$OMP_PROC_BIND
+export OMP_DISPLAY_ENV=$OMP_DISPLAY_ENV
+export OMP_DISPLAY_AFFINITY=$OMP_DISPLAY_AFFINITY
 
 # Clearing shared memory allocated by `PetscShmgetAllocateArray()`
 ipcrm --all
