be5cb82f023f3f5e7fdfc40e26d51a3545401b62
vakurshakov <crimsonschist@gmail.com>
2025-03-19 12:34:21 +0700

    minor changes

--- src/utils/configuration.h
+++ src/utils/configuration.h
@@ -3,7 +3,7 @@
 
 #include <filesystem>
 
-#include <petscdm.h>  // for DMBoundaryType
+#include <petscdmtypes.h>  // For DMBoundaryType
 
 #include <nlohmann/json.hpp>
 
@@ -26,21 +26,15 @@ public:
   static const Configuration& get();
 
   /**
-   * @brief Initializes configuration class instance.
-   * Should be used before any get/save operations.
+   * @brief Initializes configuration class instance. Should be used before any get/save operations.
    * @param config_path Location of json file, provided by argv[1].
    */
   static void init(const std::string& config_path);
 
-  /**
-   * @brief Stores the configuration json file.
-   * Overwrites the existing one, if present.
-   */
+  /// @brief Stores the configuration json file. Overwrites the existing one, if present.
   static void save();
 
-  /**
-   * @brief Stores the entire `src/` directory.
-   */
+  /// @brief Stores the entire `src/` directory. Overwrites the existing one, if present.
   static void save_sources();
 
   /// @brief Getter of boundaries type, reads the config at
@@ -48,9 +42,8 @@ public:
   static void get_boundaries_type(
     DMBoundaryType& bx, DMBoundaryType& by, DMBoundaryType& bz);
 
-  /// @brief Getter of processors number, reads the config at
-  /// Geometry.da_processors_{x, y, z}.
-  static void get_processors(PetscInt& px, PetscInt& py, PetscInt& pz);
+  static void get_processors(  //
+    PetscInt& px, PetscInt& py, PetscInt& pz);
 
 private:
   std::string config_path_;
