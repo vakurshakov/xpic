663ee566ab2e5140414059f9b789ad10f1690d9a
vakurshakov <crimsonschist@gmail.com>
2025-05-13 15:38:05 +0700

    compare.py -> comparison.py: to be the same as the output directory

--- src/commands/kotelnikov_equilibrium/cyl_without_phi/py-scripts/comparison.py
+++ src/commands/kotelnikov_equilibrium/cyl_without_phi/py-scripts/comparison.py
@@ -4,7 +4,7 @@ import sys
 import struct
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).parents[5])) 
+sys.path.append(str(Path(__file__).parents[5]))
 
 from tools.plotting.plots.plot import *
 
@@ -21,6 +21,7 @@ n = gen_plot("$n$", "ions/density_planeZ_0001", 'Z', 0, 1, FieldView.Cartesian, 
 ja = gen_plot("$J_{\\phi}$", "ions/current_planeZ_0001", 'Z', 1, 3, FieldView.Cylindrical, vmap_j, signed_cmap)
 prr = gen_plot("$\\Pi_{rr}$", "ions/momentum_flux_diag_cyl_planeZ_0001", 'Z', 0, 3, FieldView.Cartesian, vmap_p, unsigned_cmap)
 paa = gen_plot("$\\Pi_{\\phi\\phi}$", "ions/momentum_flux_diag_cyl_planeZ_0001", 'Z', 1, 3, FieldView.Cartesian, vmap_p, unsigned_cmap)
+pzz = gen_plot("$\\Pi_{\\phi\\phi}$", "ions/momentum_flux_diag_cyl_planeZ_0001", 'Z', 2, 3, FieldView.Cartesian, vmap_p, unsigned_cmap)
 bz = gen_plot("$B_z$", "B_planeZ_0001", 'Z', 2, 3, FieldView.Cylindrical, vmap_b, unsigned_cmap)
 
 plots = (n, ja, prr, bz)
@@ -29,6 +30,7 @@ n_av = gen_linear("$n$", 'Z', vmap_n, linewidth=3)
 ja_av = gen_linear("$J_{\\phi}$", 'Z', vmap_j, linewidth=3)
 prr_av = gen_linear("\\rm Pressures", 'Z', vmap_p, linewidth=3)
 paa_av = gen_linear("", 'Z', vmap_p, linewidth=3)
+pzz_av = gen_linear("", 'Z', vmap_p, linewidth=3)
 bz_av = gen_linear("$B_z$", 'Z', vmap_b, linewidth=3)
 
 plots_av = (n_av, ja_av, prr_av, bz_av)
@@ -36,7 +38,7 @@ plots_av = (n_av, ja_av, prr_av, bz_av)
 def get_theory(name):
     cache = Path(__file__).parent / f"../cache/a1.5_B0.01/maxw_{name}"
     with open(cache, "rb") as file:
-        buff = file.read() 
+        buff = file.read()
         return np.asarray(struct.unpack('d' * (len(buff) // 8), buff))
 
 data = get_theory("n")
@@ -66,13 +68,17 @@ def callback(t):
         ax = av.axis
         if p == prr:
             paa.data = paa.view.parse(t)
+            pzz.data = pzz.view.parse(t)
             paa_av.data = phi_averaged(paa.data, const.rmap)
+            pzz_av.data = phi_averaged(pzz.data, const.rmap)
 
             ax.plot(rs, prr_av.data, **prr_av.plot_info, label="$\\Pi_{rr}$")
             ax.plot(rth, prr_th, **prr_av.plot_info, linestyle="--", label="$\\Pi_{rr}^{th}$")
 
             ax.plot(rs, paa_av.data, **paa_av.plot_info, label="$\\Pi_{\\phi\\phi}$")
             ax.plot(rth, paa_th, **paa_av.plot_info, linestyle="--", label="$\\Pi_{\\phi\\phi}^{th}$")
+
+            ax.plot(rs, pzz_av.data, **pzz_av.plot_info, label="$\\Pi_{zz}$")
 
             ax.legend(fontsize=ticksize)
         else:
