4230f6b647b6b5d4e73dc3cf3faeafafc4458789
vakurshakov <crimsonschist@gmail.com>
2025-05-19 11:24:01 +0700

    diagnose period for SimulationBackup is in [dt] units

--- src/diagnostics/builders/simulation_backup_builder.cpp
+++ src/diagnostics/builders/simulation_backup_builder.cpp
@@ -27,11 +27,8 @@ PetscErrorCode SimulationBackupDiagBuilder::build(
   const Configuration::json_t& info)
 {
   PetscFunctionBeginUser;
-  PetscReal dp_wp;
-  info.at("diagnose_period").get_to(dp_wp);
-
-  PetscInt dp = ROUND_STEP(dp_wp, dt);
-  LOG("  Simulation backup diagnostic is added, diagnose period: {:.1f} [1/w_pe], {} [dt]", dp_wp, dp);
+  PetscReal dp;
+  info.at("diagnose_period").get_to(dp);
 
   auto res_dir = CONFIG().out_dir + "/simulation_backup";
   CONFIG().save(res_dir);
@@ -40,6 +37,8 @@ PetscErrorCode SimulationBackupDiagBuilder::build(
     CONFIG().save_sources(res_dir + "/src");
 
   diagnostics_.emplace_back(make_simulation_backup(simulation_, dp));
+
+  LOG("  Simulation backup diagnostic is added, diagnose period: {:.1f} [1/w_pe], {} [dt]", dp * dt, dp);
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
