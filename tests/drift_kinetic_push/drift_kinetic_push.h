523ccd0923ce539f078086a6efb1273a0106673e
vakurshakov <crimsonschist@gmail.com>
2025-10-02 16:43:13 +0700

    drift_kinetic_push_ex9 is added to compare the behavior of the pusher with the other ones: 1) correct regime is chosen using low value of v_abs (rho * |(nabla B) / B| << 1) 2) testing the different values of pitch angles, i.e. both lost and trapped particles 3) re-directing the test from ctest into python script to both run the binary and draw the trajectories!

--- tests/drift_kinetic_push/drift_kinetic_push.h
+++ tests/drift_kinetic_push/drift_kinetic_push.h
@@ -10,3 +10,87 @@ PetscErrorCode get_omega_dt(PetscReal& omega_dt)
   PetscCheck(flg, PETSC_COMM_WORLD, PETSC_ERR_USER, "Must specify the timestep (Omega * dt) with '-omega_dt'");
   PetscFunctionReturn(PETSC_SUCCESS);
 }
+
+namespace magnetic_mirror {
+
+constexpr PetscReal B_min = 1.0;
+constexpr PetscReal B_max = 4.0;
+constexpr PetscReal L = 5.0;      // Half the length of the trap
+constexpr PetscReal D = 1.0;      // Mirror width
+constexpr PetscReal S = POW2(D);  // Mirror width squared
+
+inline PetscReal exp(PetscReal z, PetscReal z0)
+{
+  return std::exp(-POW2(z - z0) / S);
+}
+
+// Center field profile on the axis (double gauss)
+PetscReal get_Bz(PetscReal z)
+{
+  return B_min + (B_max - B_min) * (exp(z, -L) + exp(z, +L));
+}
+
+PetscReal get_dBz_dz(PetscReal z)
+{
+  return (B_max - B_min) * //
+    ((-2.0 * (z + L) / S * exp(z, -L)) + //
+      (-2.0 * (z - L) / S * exp(z, +L)));
+}
+
+PetscReal get_d2Bz_dz2(PetscReal z)
+{
+  PetscReal t1 = (z + L);
+  PetscReal t2 = (z - L);
+  return (B_max - B_min) * //
+    ((-2.0 / S + 4.0 * POW2(t1 / S)) * exp(z, -L) + //
+      (-2.0 / S + 4.0 * POW2(t2 / S)) * exp(z, +L));
+}
+
+PetscReal get_d3Bz_dz3(PetscReal z)
+{
+  PetscReal t1 = (z + L);
+  PetscReal t2 = (z - L);
+  return (B_max - B_min) * //
+    ((12.0 * t1 / (S * S) - 8.0 * POW3(t1 / S)) * exp(z, -L) + //
+      (12.0 * t2 / (S * S) - 8.0 * POW3(t2 / S)) * exp(z, +L));
+}
+
+// Bz field off axis
+PetscReal get_Bz_corr(const Vector3R& r)
+{
+  return get_Bz(r.z()) - 0.25 * (POW2(r.x()) + POW2(r.y())) * get_d2Bz_dz2(r.z());
+}
+
+void get_fields(const Vector3R& pos, Vector3R&, Vector3R& B_p, Vector3R& gradB_p)
+{
+  PetscReal x = pos.x();
+  PetscReal y = pos.y();
+  PetscReal z = pos.z();
+  PetscReal r2 = x * x + y * y;
+  PetscReal r = std::sqrt(r2);
+
+  // 1) Calculation of the axial field and its derivatives
+  PetscReal Bz = get_Bz(z);
+  PetscReal dBz_dz = get_dBz_dz(z);
+  PetscReal d2Bz_dz2 = get_d2Bz_dz2(z);
+  PetscReal d3Bz_dz3 = get_d3Bz_dz3(z);
+
+  // 2) Computation of the magnetic field vector B_p in the paraxial
+  // approximation: transverse components, follow from div(B) = 0,
+  // longitudinal component with second-order correction by radius.
+  B_p = Vector3R{
+    -0.5 * x * dBz_dz,
+    -0.5 * y * dBz_dz,
+    Bz - 0.25 * r2 * d2Bz_dz2,
+  };
+
+  // 3) Calculation of the field modulus gradient |B|
+  PetscReal dB_dr = -0.5 * r * d2Bz_dz2;
+  PetscReal dB_dz = dBz_dz - 0.25 * r2 * d3Bz_dz3;
+
+  gradB_p = (r > 1e-12) //
+    ? Vector3R{(x / r) * dB_dr, (y / r) * dB_dr, dB_dz}
+    : Vector3R{0, 0, dB_dz};
+}
+
+}  // namespace magnetic_mirror
---
+++ tests/drift_kinetic_push/drift_kinetic_push_ex9.cpp
@@ -0,0 +1,112 @@
+#include "drift_kinetic_push.h"
+
+#include "src/algorithms/boris_push.h"
+#include "src/algorithms/crank_nicolson_push.h"
+
+static constexpr char help[] =
+  "Magnetic mirror field with realistic analytic mirrors, includes the\n"
+  "comparison between different pushers. Sweep in critical pitch angle\n"
+  "fraction and pusher time step (in Omega * dt units) is added.\n";
+
+int main(int argc, char** argv)
+{
+  using namespace magnetic_mirror;
+
+  PetscFunctionBeginUser;
+  PetscCall(PetscInitialize(&argc, &argv, NULL, help));
+
+  PetscReal mirror_R = get_Bz(0) / get_Bz(L);
+
+  PetscReal pitch_frac = 1.005;
+  PetscCall(PetscOptionsGetReal(NULL, NULL, "-pitch_frac", &pitch_frac, NULL));
+
+  PetscReal v_abs = 0.1;
+  PetscReal v_pitch = pitch_frac * asin(sqrt(mirror_R));
+  PetscReal v_par = v_abs * cos(v_pitch);
+  PetscReal v_perp = v_abs * sin(v_pitch);
+
+  Vector3R r0(0.1, 0, 0);
+  Vector3R v0(v_perp, 0, v_par);
+
+  PetscReal Omega_dt = 0.1;
+  PetscCall(PetscOptionsGetReal(NULL, NULL, "-omega_dt", &Omega_dt, NULL));
+
+  PetscReal Omega = get_Bz(r0.z());
+  dt = Omega_dt / Omega;
+  geom_nt = 5000;
+  diagnose_period = 1;
+
+  Vector3R B{
+    -0.5 * r0.x() * get_dBz_dz(r0.z()),
+    -0.5 * r0.y() * get_dBz_dz(r0.z()),
+    get_Bz_corr(r0),
+  };
+
+#if 0
+  Vector3R gradB{
+    -0.5 * r0.x() * get_d2Bz_dz2(r0.z()),
+    -0.5 * r0.y() * get_d2Bz_dz2(r0.z()),
+    get_dBz_dz(r0.z()) - 0.25 * hypot(r0.x(), r0.y()) * get_d3Bz_dz3(r0.z()),
+  };
+
+  PetscReal rho = v_perp / B.length();
+
+  LOG("Mirror parameters:");
+  LOG("  half-length = {}, width = {}", L, D);
+  LOG("  field = {}, {}, {}", B.x(), B.y(), B.z());
+  LOG("  field gradients = {}, {}, {}", abs(gradB.x() / B.x()), abs(gradB.y() / B.y()), abs(gradB.z() / B.z()));
+  LOG("Particle parameters:");
+  LOG("  larmor radius = {}", rho);
+  LOG("  drift approximation = {}, {}, {}", rho * abs(gradB.x() / B.x()), rho * abs(gradB.y() / B.y()), rho * abs(gradB.z() / B.z()));
+#endif
+
+  Vector3R dB_p;
+
+  Point b_p(r0, v0);
+  BorisPush b_push;
+  b_push.set_qm(1.0);
+
+  Point cn_p(r0, v0);
+  CrankNicolsonPush cn_push;
+  cn_push.set_qm(1.0);
+  cn_push.set_fields_callback([&](const Vector3R& r0, const Vector3R& r1, Vector3R& E, Vector3R& B) {
+    get_fields((r1 + r0) / 2, E, B, dB_p);
+  });
+
+  Vector3R R = r0 + v0.cross(B) / Omega;
+
+  PointByField dk_p({R, v0}, {0.0, 0.0, get_Bz_corr(R)}, 1.0);
+  DriftKineticPush dk_push;
+  dk_push.set_qm(1.0);
+  dk_push.set_mp(1.0);
+  dk_push.set_fields_callback(get_fields);
+
+  auto format = [&](const char* push) {
+    return std::format("{}_omega_dt_{:.1f}_pf_{:.3f}", push, Omega_dt, pitch_frac);
+  };
+
+  PointTrace b_d(__FILE__, format("boris"), b_p);
+  PointTrace cn_d(__FILE__, format("crank_nicolson"), cn_p);
+  PointByFieldTrace dk_d(__FILE__, format("drift_kinetic"), dk_p);
+
+  for (PetscInt t = 0; t <= geom_nt; ++t) {
+    Vector3R E_p, B_p;
+    b_push.update_r(dt / 2.0, b_p);
+    get_fields(b_p.r, E_p, B_p, dB_p);
+    b_push.set_fields(E_p, B_p);
+    b_push.update_vB(dt, b_p);
+    b_push.update_r(dt / 2.0, b_p);
+    PetscCall(b_d.diagnose(t));
+
+    Point cn_p0 = cn_p;
+    cn_push.process(dt, cn_p, cn_p0);
+    PetscCall(cn_d.diagnose(t));
+
+    PointByField dk_p0 = dk_p;
+    dk_push.process(dt, dk_p, dk_p0);
+    PetscCall(dk_d.diagnose(t));
+  }
+
+  PetscCall(PetscFinalize());
+  return EXIT_SUCCESS;
+}
