9209cb1989497a0263191ce0da7c2422dca7dcb9
vakurshakov <crimsonschist@gmail.com>
2025-10-03 12:05:24 +0700

    placing everything into a single process method

--- src/algorithms/crank_nicolson_push.cpp
+++ src/algorithms/crank_nicolson_push.cpp
@@ -35,14 +35,29 @@ void CrankNicolsonPush::process(PetscReal dt, Point& pn, const Point& p0)
 
   PetscReal rn = 0, r0 = 0;
 
-  update_r(dt, pn, p0);
+  Vector3R vh = 0.5 * (pn.p + p0.p);
+
+  auto calc_residue = [&] {
+    return ((pn.p - p0.p) / dt - qm * (E_p + vh.cross(B_p))).length();
+  };
+
   set_fields(pn.r, p0.r, E_p, B_p);
-  r0 = get_residue(dt, pn, p0);
+  r0 = calc_residue();
+
+  PetscReal alpha = 0.5 * dt * qm;
+  Vector3R a, b, w;
 
   for (it = 0; it < maxit; ++it) {
-    update_v(dt, pn.p, p0.p);
-    update_r(dt, pn, p0);
-    rn = get_residue(dt, pn, p0);
+    a = alpha * E_p;
+    b = alpha * B_p;
+
+    w = p0.p + a;
+    vh = (w + w.cross(b) + b * w.dot(b)) / (1.0 + b.squared());
+
+    pn.r = p0.r + dt * vh;
+    pn.p = 2.0 * vh - p0.p;
+
+    rn = calc_residue();
 
     if (rn < atol + rtol * r0)
       return;
@@ -52,29 +67,4 @@ void CrankNicolsonPush::process(PetscReal dt, Point& pn, const Point& p0)
 
   PetscCheckAbort(rn >= atol + rtol * r0, PETSC_COMM_WORLD, PETSC_ERR_USER,
     "CrankNicolsonPush::process() nonlinear iterations diverged with norm %e!", rn);
-}
-
-void CrankNicolsonPush::update_v(
-  PetscReal dt, Vector3R& vn, const Vector3R& v0) const
-{
-  PetscReal alpha = 0.5 * dt * qm;
-  Vector3R a = alpha * E_p;
-  Vector3R b = alpha * B_p;
-
-  Vector3R w = v0 + a;
-  Vector3R vh = (w + w.cross(b) + b * w.dot(b)) / (1.0 + b.squared());
-
-  vn = 2.0 * vh - v0;
-}
-
-void CrankNicolsonPush::update_r(PetscReal dt, Point& pn, const Point& p0) const
-{
-  pn.r = p0.r + (0.5 * dt) * (pn.p + p0.p);
-}
-
-PetscReal CrankNicolsonPush::get_residue(
-  PetscReal dt, const Point& pn, const Point& p0) const
-{
-  Vector3R vh = 0.5 * (pn.p + p0.p);
-  return ((pn.p - p0.p) / dt - qm * (E_p + vh.cross(B_p))).length();
 }
